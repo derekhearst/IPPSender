ae88b980b31fb98a5f60a241a4cd104562ee00d5
Derek Hearst <derekallenhearst@gmail.com>
2021-09-08 15:26:49 -0600

    Setup and themes

---
+++ IPPSender/UI/ItemAdder.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.IO;
+
+namespace IPPSender
+{
+	/// <summary>
+	/// Interaction logic for ItemAdder.xaml
+	/// </summary>
+	public partial class ItemAdder : UserControl
+	{
+		public ItemAdder()
+		{
+			InitializeComponent();
+		}
+
+		public void AddSearchFolder(DirectoryInfo directory)
+		{
+			
+			items.ItemsSource = directory.GetFiles();
+		}
+	}
+}
